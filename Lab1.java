febdc6a265b1c677ea981b0ec1ecb888a06af088
Vorciu <60293261+Vorciu@users.noreply.github.com>
2020-05-03 15:12:46 +0200

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,48 @@
+public class Lab1 {
+
+   static class MyThread extends Thread{
+        
+        final int threadId; 
+        MyThread waitForThread; 
+        
+        MyThread(int threadId){
+            this.threadId = threadId;
+        }
+        
+        @Override
+         public void run() 
+         { 
+            try {
+                if(waitForThread != null)
+                    waitForThread.join();  
+            } catch (InterruptedException ex) {
+                
+            }
+            System.out.println ("Thread " + threadId + " is running"); 
+        
+         } 
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        MyThread tab[] = new MyThread[10];
+        for(int i = 0; i < 10 ; i++){
+            
+             tab[i] = new MyThread(i);
+             if(i>0)
+                tab[i-1].waitForThread = tab[i];
+             
+                 
+            
+        }
+        
+        for(int i = 0; i <10 ; i++){
+            
+            tab[i].start();
+        }
+        
+       
+    }
+    
+}
